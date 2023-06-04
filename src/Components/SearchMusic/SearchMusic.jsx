14b7e4cacecc677b1eb0bea018f0ca7f71320c9d
unknown <sean.herbison@gmail.com>
2023-06-04 16:12:54 -0700

    Added Search dropdown for filtering.

--- src/Components/SearchMusic/SearchMusic.jsx
+++ src/Components/SearchMusic/SearchMusic.jsx
@@ -4,7 +4,9 @@ import styles from "./SearchMusic.module.css";
 
 function SearchMusic() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [filterCategory, setFilterCategory] = useState("All");
   const [musicData, setMusicData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,16 +25,67 @@ function SearchMusic() {
     setSearchTerm(event.target.value);
   };
 
+  const handleFilterChange = (event) => {
+    setFilterCategory(event.target.value);
+  };
+
+  const handleSearch = () => {
+    let filtered = musicData.filter((song) => {
+      let searchString = "";
+      switch (filterCategory) {
+        case "All":
+          searchString =
+            song.title.toLowerCase() +
+            song.artist.toLowerCase() +
+            song.album.toLowerCase() +
+            song.genre.toLowerCase();
+          break;
+        case "Title":
+          searchString = song.title.toLowerCase();
+          break;
+        case "Artist":
+          searchString = song.artist.toLowerCase();
+          break;
+        case "Album":
+          searchString = song.album.toLowerCase();
+          break;
+        case "Genre":
+          searchString = song.genre.toLowerCase();
+          break;
+        
+      }
+      return searchString.includes(searchTerm.toLowerCase());
+    });
+    setFilteredData(filtered);
+  };
+
   return (
     <div className={styles.formContainer}>
-      <input
-        id="searchInput"
-        type="text"
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={handleInputChange}
-      />
-      <button id="searchButton">Search</button>
+      <div className={styles.searchContainer}>
+        <input
+          id="searchInput"
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+
+        <select
+          id="filterCategory"
+          value={filterCategory}
+          onChange={handleFilterChange}
+        >
+          <option value="All">All</option>
+          <option value="Title">Title</option>
+          <option value="Artist">Artist</option>
+          <option value="Album">Album</option>
+          <option value="Genre">Genre</option>
+        </select>
+
+        <button id="searchButton" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
 
       <table className={styles.musicTable}>
         <thead>
@@ -48,7 +101,7 @@ function SearchMusic() {
           </tr>
         </thead>
         <tbody>
-        {/* Ternary operation - {musicData.length > 0 ? (map music) : ("No data yet"}
+          {/* Ternary operation - {musicData.length > 0 ? (map music) : ("No data yet"}
         Hard to read when spaced out like below. */}
           {musicData.length > 0 ? (
             musicData.map((song) => (
@@ -64,7 +117,8 @@ function SearchMusic() {
             ))
           ) : (
             <tr>
-              <td colSpan="7">No data yet</td> {/* Change after making buttons */}
+              <td colSpan="7">No data yet</td>{" "}
+              {/* Change after making buttons */}
             </tr>
           )}
         </tbody>
